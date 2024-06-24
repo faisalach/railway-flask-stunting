4959decd1748b30a87d63f07b85516fb268fd45b
faisalach <faisalachramsyah@gmail.com>
2024-06-24 18:35:40 +0700

    fix index

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from flask import Flask
-app = Flask(__name__)
- 
-@app.route('/')
-def hello_world():
-    return 'Hello from Koyeb'
- 
- 
-if __name__ == "__main__":
-    app.run()
+from flask import Flask, jsonify
+import os
 
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
