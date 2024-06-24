68ff0eea3331f063bee0101ad208ea500b3f86ea
faisalach <faisalachramsyah@gmail.com>
2024-06-24 18:56:25 +0700

    add route food

--- main.py
+++ main.py
@@ -13,6 +13,23 @@ CORS(app)
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
+@app.route("/api/foods",methods=['POST'])
+def food():
+    food_ids    = request.json['food_ids']
+    umur    = request.json['umur']
+    jenis_kelamin    = request.json['jenis_kelamin']
+    tinggi_badan    = request.json['tinggi_badan']
+
+    predict = predict_status_gizi(umur, jenis_kelamin, tinggi_badan)
+    result  = recommend_foods(food_ids)
+    return jsonify({
+        'food' : result,
+        'stunting': {
+            'stunting': predict,
+            'umur': umur,
+            'jenis_kelamin': jenis_kelamin,
+            'tinggi_badan': tinggi_badan
+        }})
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
